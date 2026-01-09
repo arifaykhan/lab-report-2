e69abebd42c921e905cc37a51ff82211d2ac8958
Arif Aykhan Mammadli <79666195+arifaykhan@users.noreply.github.com>
2026-01-09 16:40:36 +0400

    Update task_12.5_slave.ino

--- task_12.5_slave.ino
+++ task_12.5_slave.ino
@@ -1,41 +1,55 @@
+// slave board
+
 void setup() {
   // put your setup code here, to run once:
-  DDRB |= (1 << 5);
-  DDRD &= ~(1 << 2);
-  PORTD |= (1 << 2);
-
+  DDRB |= (1 << PB0);
+  DDRD &= ~(1 << PD2);
+  
   TWAR = (0x08 << 1);
-  PORTC |= (1 << 4) | (1 << 5);
-  TWCR = (1 << TWEA) | (1 << TWEN) | (1 << TWINT);
+  
+  DDRC &= ~((1 << PC4) | (1 << PC5));
+  PORTC |= (1 << PORTC4) | (1 << PORTC5);
+
+  TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (TWCR & (1 << TWINT)) {
-    uint8_t status = TWSR & 0xF8;
+  while (!(TWCR & (1 << TWINT)));
 
-    if (status == 0x60) {
+  uint8_t status = (TWSR & B11111000);
+
+  switch (status) {
+    case B01100000: 
       TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
-    } 
-    else if (status == 0x80) {
-      uint8_t receivedData = TWDR;
-      if (receivedData) {
-        PORTB |= (1 << 5);
+      break;
+    case B10000000: 
+      if (TWDR == 1) {
+        PORTB |= (1 << PB0);
       } else {
-        PORTB &= ~(1 << 5);
+        PORTB &= ~(1 << PB0);
       }
       TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
-    }
-    else if (status == 0xA8) {
-      uint8_t buttonState = !(PIND & (1 << 2));
-      TWDR = buttonState;
+      break;
+    case B10101000: 
+      if (PIND & (1 << PD2)) { 
+        TWDR = 1; // Send "Pressed"
+      } else {
+        TWDR = 0; // Send "Released"
+       }
+       TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
+       break;
+    case B11000000: 
       TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
-    }
-    else if (status == 0xB8) {
+      break;
+    case B10111000:
       TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
-    }
-    else {
+      break;
+    case B10100000:
       TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
-    }
+      break;
+    default:
+      TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
+      break;
   }
 }
