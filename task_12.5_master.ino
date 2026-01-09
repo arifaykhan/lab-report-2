1f8e8fd6910f1fc221d5e36a9552fe2596e8b1fc
Arif Aykhan Mammadli <79666195+arifaykhan@users.noreply.github.com>
2026-01-09 16:38:59 +0400

    Update task_12.5_master.ino

--- task_12.5_master.ino
+++ task_12.5_master.ino
@@ -1,47 +1,53 @@
+// master board3
+
 void setup() {
   // put your setup code here, to run once:
-  DDRB |= (1 << 5);
-  DDRD &= ~(1 << 2);
-  PORTD |= (1 << 2);
-
+  DDRB |= (1 << PB0);
+  DDRD &= ~(1 << PD2);
+  
+  TWSR &= ~((1 << TWPS0 | (1 << TWPS1)));
   TWBR = 72;
-  PORTC |= (1 << 4) | (1 << 5);
-  TWCR = (1 << TWEN);
+
+  DDRC &= ~((1 << PC4) | (1 << PC5));
+  PORTC |= (1 << PORTC4) | (1 << PORTC5);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  uint8_t buttonState = !(PIND & (1 << 2));
+  uint8_t button = (PIND & (1 << PD2)) ? 1 : 0;
+  uint8_t slavebutton = 0;
   
   TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
   while (!(TWCR & (1 << TWINT)));
-  
-  TWDR = (0x08 << 1) | 0;
-  TWCR = (1 << TWINT) | (1 << TWEN);
+
+  TWDR = (0x08 << 1) | 0; // Load data
+  TWCR = (1 << TWINT) | (1 << TWEN); // Start transmission
   while (!(TWCR & (1 << TWINT)));
-  
-  TWDR = buttonState;
-  TWCR = (1 << TWINT) | (1 << TWEN);
+
+  TWDR = button; // Load data
+  TWCR = (1 << TWINT) | (1 << TWEN); // Start transmission
   while (!(TWCR & (1 << TWINT)));
-  
+
   TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);
 
   TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
   while (!(TWCR & (1 << TWINT)));
-  
-  TWDR = (0x08 << 1) | 1;
-  TWCR = (1 << TWINT) | (1 << TWEN);
+
+  TWDR = (0x08 << 1) | 1; // Load data
+  TWCR = (1 << TWINT) | (1 << TWEN); // Start transmission
   while (!(TWCR & (1 << TWINT)));
-  
-  TWCR = (1 << TWINT) | (1 << TWEN);
+
+  TWCR = (1 << TWINT) | (1 << TWEN); 
   while (!(TWCR & (1 << TWINT)));
-  uint8_t receivedData = TWDR;
-  
+
+  slavebutton = TWDR;
+
   TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);
 
-  if (receivedData) {
-    PORTB |= (1 << 5);
+  if (slavebutton == 1) {
+    PORTB |= (1 << PB0);  // LED ON
   } else {
-    PORTB &= ~(1 << 5);
+    PORTB &= ~(1 << PB0); // LED OFF
   }
+  delay(100);
 }
